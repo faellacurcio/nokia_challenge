46d708b0fdc1be8ffd1a47aec86d14e84d91abce
Rafael <rafael.acurcio@gmail.com>
2020-11-05 04:05:10 +0100

    Encapsulating Runner class;

---
+++ src/com/company/Runner.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.sql.Connection;
+import java.util.HashMap;
+
+public class Runner {
+    private Connection conn;
+
+    public Runner(Connection conn) {
+        this.conn = conn;
+    }
+
+    public static void runCommand(String command, HashMap flags){
+
+    }
+    public static void parseCommand(String raw_command){
+
+    }
+}
